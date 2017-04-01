5e6ce08a04c306c38b09bd0645c178012f7e9e9c
likailun <likailun@gznb.com>
2017-04-01 14:20:52 +0800

    删除测试方法

--- src/main/java/com/newbee/poi/config/AsyncTaskService.java
+++ src/main/java/com/newbee/poi/config/AsyncTaskService.java
@@ -56,7 +56,4 @@ public class AsyncTaskService implements AsyncConfigurer, SchedulingConfigurer {
         scheduledTaskRegistrar.setTaskScheduler(scheduler);
     }
 
-    public void test(){
-
-    }
 }
